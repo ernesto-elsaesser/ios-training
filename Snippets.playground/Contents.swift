1692487a824db5f20011049964b05631e6a787b5
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2018-08-13 20:28:29 +0200

    simplify Snippets playground

--- Snippets.playground/Contents.swift
+++ Snippets.playground/Contents.swift
@@ -1,44 +1,52 @@
 import PlaygroundSupport
 import UIKit
 
-// timer
+let demo = "LIVEVIEW"
 
-/*
-PlaygroundPage.current.needsIndefiniteExecution = true
-let timer = MyTimer()
-timer.start()
- */
-
-
-// number formatting
-
-/*
-let d = 3.1415
-let formatter = MyFormatter()
-formatter.format1(number: d)
-formatter.format2(number: d)
- */
-
-// file IO
-
-/*
-let io = FileIO()
-io.write(text: "TEXT")
-io.read()
-
-let data = "DATA".data(using: .utf8)!
-io.write(data: data)
-io.read()
- */
-
-// network call
-
-/*
-let loader = DataLoader()
-loader.loadZen()
- */
-
-// view controller
-
-let vc = DrawingViewController()
-PlaygroundPage.current.liveView = vc
+switch demo {
+    
+case "TIMER":
+    
+    PlaygroundPage.current.needsIndefiniteExecution = true
+    _ = Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { timer in
+        let endDate = Date()
+        print("\(endDate) Timer fired.")
+    }
+    let startDate = Date()
+    print("\(startDate) Timer scheduled.")
+    
+case "NUMFORMAT":
+    
+    let d = 3.1415
+    
+    let str1 = String(format: "%.3f", d)
+    print(str1)
+    
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .currency
+    numberFormatter.maximumFractionDigits = 2
+    numberFormatter.currencyCode = "EUR"
+    //numberFormatter.locale = Locale(identifier: "de-DE")
+    
+    let str2 = numberFormatter.string(from: d as NSNumber)!
+    print(str2)
+    
+case "FILEIO":
+    
+    let io = FileIO()
+    io.write(text: "TEXT")
+    io.read()
+    
+    let data = "DATA".data(using: .utf8)!
+    io.write(data: data)
+    io.read()
+    
+case "LIVEVIEW":
+    
+    let vc = DrawingViewController()
+    PlaygroundPage.current.liveView = vc
+    
+default:
+    break
+    
+}
